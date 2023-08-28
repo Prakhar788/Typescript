8b9504d733eff98f354c29859660fb31e33cbdd8
Prakhar Sharma <prakhar.sharma_cs20@gla.ac.in>
2023-08-28 18:11:13 +0530

    Adding New Files

--- todo-ts/src/main.ts
+++ todo-ts/src/main.ts
@@ -29,10 +29,38 @@ renderTodo(todos);
 }
 
 
-const
+const generate=(title:string,isCompleted:boolean,id:string)=>{
+ const todo:HTMLDivElement=document.createElement("div");
+ todo.className="todo";
+ const checkBox:HTMLInputElement=document.createElement("input")
+ checkBox.setAttribute("type","checkbox");
+ checkBox.className="isCompleted";
+ checkBox.checked=isCompleted;
+
+
+const para:HTMLParagraphElement=document.createElement("p");
+para.innerText=title;
+
+const btn:HTMLButtonElement=document.createElement("button");
+btn.innerText="Delete";
+btn.className="deleteBtn"
+btn.onclick=()=>{
+  deleteTodo(id);
+  renderTodo(todos);
+};
+
+const deleteTodo=(id:string)=>{
+const indx=todos.findIndex((item)=>item.id===id);
+todos.splice(indx,1);
+}
+
+todo.append(checkBox,para,btn);
+todosContainer.append(todo);
+}
 
 const renderTodo=(todos:Todo[])=>{
+  todosContainer.innerText="";
 todos.forEach((item)=>{
-  generate();
+  generate(item.title,item.isCompleted,item.id);
 })
 }

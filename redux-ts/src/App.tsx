7a7f529525d73eff6f1a081c2aa98104a0e0419f
Prakhar Sharma <prakhar.sharma_cs20@gla.ac.in>
2023-08-30 19:24:46 +0530

    Adding New Files

--- redux-ts/src/App.tsx
+++ redux-ts/src/App.tsx
@@ -1,4 +1,6 @@
+const incbyValue=()=>{
 
+}
 
 const App = () => {
   return (
--- redux-ts/src/redux.ts
+++ redux-ts/src/redux.ts
@@ -1,7 +1,59 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore,createReducer,createAction } from "@reduxjs/toolkit";
+
+// interface StateType{
+//     count:number;
+// }
+// const initialState:StateType={
+//     count:0
+// };
+// const increment=createAction("increment");
+// const decrement=createAction("decrement");
+// const rootReducer=createReducer(initialState,(builder)=>{
+// builder.addCase(increment,(state)=>{
+//     state.count+=1;
+// }).addCase(decrement,(state)=>{
+//     state.count-=1;
+// })
+// })
+
+
+// export const store=configureStore({
+//     reducer:rootReducer 
+// })
+
+
+//-------------------------------Using CreateSlice
+
+import { configureStore,createSlice,PayloadAction } from "@reduxjs/toolkit";
+
+interface StateType{
+    count:number;
+}
+const initialState:StateType={
+    count:0
+};
+
+const rootSlice=createSlice({
+    initialState:initialState,
+    name:"counter",
+    reducers:{
+        inc:(state)=>{
+            state.count+=1;
+        },
+        dec:(state)=>{
+            state.count-=1;
+        },
+        incByValue:(state,action:PayloadAction<number>)=>{
+            state.count+=action.payload;
+        }
+    }
+
+})
+
+
+export const {inc,dec,incByValue} =rootSlice.actions;
 
 export const store=configureStore({
-    reducer:{
-        
-    }
+reducer:rootSlice.reducer
+
 })

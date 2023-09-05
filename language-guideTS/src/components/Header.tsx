af30c404db52d6bd32eea928acc427f4a90b89bd
Prakhar Sharma <prakhar.sharma_cs20@gla.ac.in>
2023-09-05 23:56:18 +0530

    Language-ProjectTS

--- language-guideTS/src/components/Header.tsx
+++ language-guideTS/src/components/Header.tsx
@@ -1,8 +1,24 @@
+import { AppBar, Toolbar, Typography } from "@mui/material"
+import { Link } from "react-router-dom"
 
+
+const style={
+  color:"white",
+  margin:"0.5rem",
+  textDecoration:"none"
+}
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5" mr={"auto"} textTransform={"uppercase"}>
+          LearnOn.
+        </Typography>
+        <Link style={style} to={"/"}>Home</Link>
+        <Link style={style} to={"/login"}>Login</Link>
+      </Toolbar>
+    </AppBar>
   )
 }
 
--- language-guideTS/src/components/Home.tsx
+++ language-guideTS/src/components/Home.tsx
@@ -1,8 +1,41 @@
+import { Container, Typography,Stack, Button } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
+const languages=[{name:"Japanese",
+code:"ja",},
+{name:"Hindi",
+code:"hi",},
+{name:"Spanish",
+code:"es",},
+{name:"French",
+code:"fr",},
+]
 const Home = () => {
+   const navigate=useNavigate();
+  const langSelect=(language:string):void=>{
+  navigate(`/learn?language=${language}`)
+  }
+
+
   return (
-    <div>Home</div>
+    <Container maxWidth={"sm"}>
+      <Typography variant="h3" p={"2rem"} textAlign={"center"}>
+        Welcome,Start your Learning Journey.
+      </Typography>
+      <Stack direction={"row"} spacing={"2rem"} p={"2rem"} alignContent={"center"} justifyContent={"center"}>
+      {languages.map((i)=>(
+        <Button onClick={()=>langSelect(i.code)} key={i.code} variant="contained">{i.name}</Button>
+      ))}
+      {/* {languages.map((i)=>{
+  return <Button>{i.name}</Button>
+})} */}
+      </Stack>
+      <Typography>
+        Choose Any One Language.
+      </Typography>
+    </Container>
   )
 }
 
 export default Home
+
